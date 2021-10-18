08cf0a3daf9ccf713e6facc495b30ec40117db12
Emrehan141 <emrehan141@gmail.com>
2021-10-18 22:04:44 +0300

    WPF project added

---
+++ Neslihan_Kres_Makbuz/Config/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neslihan_Kres_Makbuz.Config
+{
+    public static class DatabaseConfig
+    {
+        private static readonly string USERNAME = "";
+        private static readonly string PASSWORD = "";
+        private static readonly string IP = "";
+        private static readonly string DB_NAME = "";
+    }
+}
