09c73bca62e93c384a0f65321ed7b86ff3d2c3b2
Emrehan141 <emrehan141@gmail.com>
2021-11-20 23:34:20 +0300

    UI improvements

--- Neslihan_Kres_Makbuz/Properties/Resources.Designer.cs
+++ Neslihan_Kres_Makbuz/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Neslihan_Kres_Makbuz.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap menu1 {
+            get {
+                object obj = ResourceManager.GetObject("menu1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
